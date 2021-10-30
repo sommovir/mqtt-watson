ddb4b82f217398626d86977b74557083103223c0
Luca <luca.coraci@gmail.com>
2021-10-30 12:14:57 +0200

    task 81 done

---
+++ src/main/java/it/cnr/istc/mw/mqtt/logic/mindgames/Prodotto.java
@@ -0,0 +1,61 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package it.cnr.istc.mw.mqtt.logic.mindgames;
+
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Luca
+ */
+public class Prodotto extends Reparto{
+    
+    private long id;
+    private String name;
+    private Reparto frist = new Reparto(id, name);
+
+    public Prodotto() {
+    }
+
+    public Prodotto(long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Reparto getFrist() {
+        return frist;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setFrist(Reparto frist) {
+        this.frist = frist;
+    }
+    
+    
+    
+
+    
+    
+    
+    
+    
+    
+    
+}
