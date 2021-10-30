c8eeeb317ea50ea1ea38ac205451f0f4f044e31f
Luca <luca.coraci@gmail.com>
2021-10-30 12:29:45 +0200

    task 111 done

---
+++ src/main/java/it/cnr/istc/mw/mqtt/logic/mindgames/GameDifficultyPolicy.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Enum.java to edit this template
+ */
+package it.cnr.istc.mw.mqtt.logic.mindgames;
+
+/**
+ *
+ * @author Luca
+ */
+public enum GameDifficultyPolicy {
+    
+    RANDOM,
+    RAISING,
+    DECRAISING;
+    
+}
