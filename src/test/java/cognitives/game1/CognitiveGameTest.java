2f3a96547e93322f8071042dffd670c15eeaecad
Luca Coraci <luca.coraci@istc.cnr.it>
2021-11-09 01:51:00 +0100

    added @Disabled at test suite

--- src/test/java/cognitives/game1/CognitiveGameTest.java
+++ src/test/java/cognitives/game1/CognitiveGameTest.java
@@ -17,12 +17,14 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 
 /**
  *
  * @author sommovir
  */
+@Disabled
 public class CognitiveGameTest {
     
     String message;
