30e1bcfa26f322ad96a5f6581a6c3dee5902e4ab
Luca Coraci <luca.coraci@gmail.com>
2022-02-18 17:04:37 +0100

    invalid reparts excepion

---
+++ src/main/java/it/cnr/istc/mw/mqtt/exceptions/InvalidRepartsExceptions.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package it.cnr.istc.mw.mqtt.exceptions;
+
+/**
+ *
+ * @author sommovir
+ */
+public class InvalidRepartsExceptions extends MindGameException {
+
+    @Override
+    public String errorMessage() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
